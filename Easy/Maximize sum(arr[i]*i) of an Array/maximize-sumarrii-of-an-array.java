4f4af966010e4716c56f330a5daf50a8320642e1
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-03 23:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize sum(arr[i]*i) of an Array/maximize-sumarrii-of-an-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            
+            System.out.println(obj.Maximize(a,n));
+        }
+        
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    
+    int Maximize(int arr[], int n)
+    {
+        long sum =0;
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            sum += (i*(long)arr[i])%1000000007;
+        }
+        return (int) (sum % 1000000007);
+        
+    }   
+}
+

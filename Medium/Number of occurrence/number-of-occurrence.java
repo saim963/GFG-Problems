fd3d5d27d3dade1262cc06754bd02e87d814e332
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-07 13:09:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.java
+++ Medium/Number of occurrence/number-of-occurrence.java
@@ -37,10 +37,27 @@ public class Main {
 
 class Solution {
     int count(int[] arr, int n, int x) {
+        /*
         int count=0;
         for(int i=0; i<n; i++){
             if(arr[i]==x)
                 count++;
+        }
+        return count;
+        */
+        //let there be two pointers and a counter
+        int start =0;
+        int end = n-1;
+        int count =0;
+        while(start<=end){
+            if(arr[start]==x){
+                count++;
+            }
+            if(arr[end]==x){
+                count++;
+                end--;
+            }
+            start++;
         }
         return count;
     }

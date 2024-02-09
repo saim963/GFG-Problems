936205adb5b7d39164f7742da3416dd6c5265b9c
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-09 22:00:17 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count Digits/count-digits.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static int evenlyDivides(int N){
+        int temp =0;
+        int count =0;
+        int og = N;
+        while(N>0){
+            temp = N%10;
+            N/=10;
+            if (temp != 0 && og % temp == 0)    count++;
+        }
+        return count;
+    }
+}

6e1d848f38c281096bfb54c2dea7a9bc0c909f41
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-13 22:43:28 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Palindrome String/palindrome-string.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String str) {
+        if (str == null || str.length() == 0) {
+            return 1;
+        }
+        str = str.toLowerCase();
+        for (int i = 0; i <= str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+
+            if (start != end) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};

b728178f822189ad96ec8104cc992713ae664aa5
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-02-20 20:00:15 +0530

    Updated Solution - GfG to GitHub

--- School/Value equal to index value/value-equal-to-index-value.java
+++ School/Value equal to index value/value-equal-to-index-value.java
@@ -40,12 +40,10 @@ public class Main {
 
 class Solution {
     ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
-        // code here
         ArrayList<Integer> list = new ArrayList<>();
         for(int i=0; i<n; i++){
             if(arr[i]==i+1)
                 list.add(arr[i]);
-            
         }
         return list;
     }

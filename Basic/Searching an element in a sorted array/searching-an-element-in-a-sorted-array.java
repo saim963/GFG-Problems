ccb28953f20e9935d906f22fcff70a6027cfb260
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-05-26 00:16:10 +0530

    Updated Solution - GfG to GitHub

--- Basic/Searching an element in a sorted array/searching-an-element-in-a-sorted-array.java
+++ Basic/Searching an element in a sorted array/searching-an-element-in-a-sorted-array.java
@@ -39,14 +39,8 @@ class gfg
 class Solution{
     static int searchInSorted(int arr[], int N, int K)
     {
-        
-        int start =0;
-        int end =N-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(arr[mid]>K) end=mid-1;
-            else if(arr[mid]<K) start=mid+1;
-            else    return 1;
+        for(int i=0; i<N; i++){
+            if(arr[i]==K)   return 1;
         }
         return -1;
     }

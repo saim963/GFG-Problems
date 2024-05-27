bca5924b85e868b329d120af38d7c34aeb2ee1d0
Saim Shakeel <87275862+saim963@users.noreply.github.com>
2024-05-28 03:05:40 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 To N Without Loop/print-1-to-n-without-loop.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    //creating an object of class Print
+		    Solution obj=new Solution();
+		    int N;
+		    
+		    //input N
+		    N=sc.nextInt();
+		    
+		    //calling printNos() methdo
+		    //of class Print
+		    obj.printNos(N);
+		    System.out.println();
+		    
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution
+{
+    
+  public void printNos(int N)
+    {
+        printer(1,N);
+    }
+    void printer(int curr, int num){
+        if(curr>num)    return;
+        System.out.print(curr+" ");
+        printer(curr+1, num);
+    }
+}
+
